db22fe5b686882747df7b3441f5adbb9b0232c7f
Sophie Borchart <sophie.borchart@ibm.com>
2023-02-24 16:29:59 -0500

    python solution for day 5 part 2

--- test_day5advent.py
+++ test_day5advent.py
@@ -48,15 +48,54 @@ class TestDay5Advent:
         return answer
         
 
-    # def day5_part2(self, input_file):
+    def day5_part2(self, input_file):
+        print('reading file: ' + input_file)
+        stack1 = ["Z", "J", "N", "W", "P", "S"]
+        stack2 = ["G", "S", "T"]
+        stack3 = ["V", "Q", "R", "L", "H"]
+        stack4 = ["V", "S", "T", "D"]
+        stack5 = ["Q", "Z", "T", "D", "B", "M", "J"]
+        stack6 = ["M", "W", "T", "J", "D", "C", "Z", "L"]
+        stack7 = ["L", "P", "M", "W", "G", "T", "J"]
+        stack8 = ["N", "G", "M", "T", "B", "F", "Q", "H"]
+        stack9 = ["R", "D", "G", "C", "P", "B", "Q", "W"]
+
+        stackOfStacks = [stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9]
+
+        regex_pattern = re.compile("move (.+) from (.+) to (.+)")
+
+        with open(input_file, 'r') as file:
+            for line in file:
+                match = regex_pattern.match(line)
+                if(match):
+                    numObjectsToMove = (int)(match.group(1))
+                    stackLosingNum = (int)(match.group(2))
+                    stackGainingNum = (int)(match.group(3))
+                    lettersToMove = []
+                    for i in range(0, numObjectsToMove):
+                        stackLosing = stackOfStacks[stackLosingNum - 1]
+                        letterToMove = stackLosing[len(stackLosing) - 1]
+                        stackLosing.pop()
+                        lettersToMove.insert(0, letterToMove)
+                        stackGaining = stackOfStacks[stackGainingNum - 1]
+                    stackGaining += lettersToMove
+        answer = ""
+        for i in range (0, len(stackOfStacks)):
+            singleStack = stackOfStacks[i]
+            answer += singleStack[len(singleStack) - 1]
+        print(answer)
+        return answer
+
+
+
 
     def test_day5_part1(self):
         assert (self.day5_part1('/Users/sophieborchart/advent_of_code/day5input.txt')) == "MQTPGLLDN"
     
-    # def test_day5_part2(self):
-    #     assert (self.day4_part2('/Users/sophieborchart/advent_of_code/day5input.txt')) == 801
+    def test_day5_part2(self):
+        assert (self.day5_part2('/Users/sophieborchart/advent_of_code/day5input.txt')) == "LVZPSTTCZ"
 
 
 obj = TestDay5Advent()
 obj.day5_part1('/Users/sophieborchart/advent_of_code/day5input.txt')
-# obj.day5_part2('/Users/sophieborchart/advent_of_code/day5input.txt')
+obj.day5_part2('/Users/sophieborchart/advent_of_code/day5input.txt')
